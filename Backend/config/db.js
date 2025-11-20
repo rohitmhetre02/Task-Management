b8d3af48712e589ef89fba7c350d6228b02d7eaa
rohitmhetre <mhetrer120@gmail.com>
2025-11-21 01:13:50 +0530

    Initial fresh commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,9 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true, useUnifiedTopology: true
     });
+    console.log('MongoDB connected');
   } catch (err) {
+    console.error('MongoDB connection error:', err && err.message ? err.message : err);
     process.exit(1);
   }
 };
